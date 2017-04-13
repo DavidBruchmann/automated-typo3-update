6a8c6c4bc53aaec686f9c179415c1c7a84200214
Daniel Siepmann <coding@daniel-siepmann.de>
2017-04-13 14:20:36 +0200

    FEATURE: Add second TypoScript check
    * Extend test to show what's possible and covered and what is not
      covered yet.
    
    Relates: #54

--- tests/Fixtures/Standards/Typo3Update/Sniffs/Removed/TypoScriptObjectIdentifierSniff/InputFileForIssues.ts
+++ tests/Fixtures/Standards/Typo3Update/Sniffs/Removed/TypoScriptObjectIdentifierSniff/InputFileForIssues.ts
@@ -2,3 +2,18 @@ page {
     10 < styles.insertContent
     10 =< styles.insertContent
 }
+
+styles.insertContent {
+    select {
+        where = colPos=1
+    }
+}
+
+# already covered
+mod.web_list.alternateBgColors = 1
+# Not covered yet
+mod {
+    web_list {
+        alternateBgColors = 1
+    }
+}
