7ef2b7c2ec850c7a10d9bcaf9e514e0e95503e1e
Daniel Siepmann <coding@daniel-siepmann.de>
2017-05-02 09:04:19 +0200

    FEATURE: Add FQObjectIdentifier to TypoScript tokens
    * To make later checks easier, add the fully qualified object
      identifier, to all object identifier tokens while tokenizing
      TypoScript.
    * Adjust tests accordingly.
    * Add necessary autoloading.
    
    Closes: #66

---
+++ src/CodeSniffer/Tokenizers/FQObjectIdentifier.php
@@ -0,0 +1,125 @@
+<?php
+namespace Typo3Update\CodeSniffer\Tokenizers;
+
+/*
+ * Copyright (C) 2017  Daniel Siepmann <coding@daniel-siepmann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+/**
+ * Update tokens with fully qualified object identifier.
+ */
+class FQObjectIdentifier
+{
+    /**
+     * Key used to save identifier to token.
+     * @var string
+     */
+    const IDENTIFIER = 'fqObjectIdentifier';
+
+    /**
+     * The fully qualified object identifier, dot separated.
+     *
+     * @var array
+     */
+    protected $fqPath = [];
+
+    /**
+     * Current "real" depth, count of opening braces.
+     * @var int
+     */
+    protected $depth = 0;
+
+    /**
+     * Add token as path segment.
+     * @param array $token
+     */
+    public function addPathSegment(array &$token)
+    {
+        $this->syncPath();
+
+        $path = [];
+        foreach (explode('.', $token['content']) as $pathSegment) {
+            $path[] = [
+                'content' => $pathSegment,
+                'depth' => $this->getDepth(),
+            ];
+        }
+        $this->fqPath = array_merge($this->fqPath, $path);
+
+        $this->addFqObjectIdentifier($token);
+    }
+
+    /**
+     * Sync path with current depth.
+     */
+    public function syncPath()
+    {
+        $this->fqPath = array_filter(
+            $this->fqPath,
+            function ($pathSegment) {
+                return $pathSegment['depth'] < $this->depth;
+            }
+        );
+    }
+
+    /**
+     * Respect opening brace internal.
+     */
+    public function handleOpeningBrace()
+    {
+        ++$this->depth;
+    }
+
+    /**
+     * Respect closing brace internal.
+     */
+    public function handleClosingBrace()
+    {
+        --$this->depth;
+        $this->syncPath();
+    }
+
+    /**
+     * @return int
+     */
+    public function getDepth()
+    {
+        return $this->depth;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        $path = '';
+        foreach ($this->fqPath as $pathSegment) {
+            $path .= '.' . $pathSegment['content'];
+        }
+
+        return substr($path, 1);
+    }
+
+    /**
+     * @param array $token
+     */
+    protected function addFqObjectIdentifier(array &$token)
+    {
+        $token[static::IDENTIFIER] = $this->getPath();
+    }
+}
---
+++ tests/CodeSniffer/Tokenizers/FqObjectIdentifierTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace Typo3Update\Tests\CodeSniffer\Tokenizers;
+
+/*
+ * Copyright (C) 2017  Daniel Siepmann <coding@daniel-siepmann.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+use Helmich\TypoScriptParser\Tokenizer\TokenInterface;
+use PHPUnit\Framework\TestCase;
+use Typo3Update\CodeSniffer\Tokenizers\FQObjectIdentifier;
+
+class FqObjectIdentifierTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function addingPathSegmentAddsFqToNewToken()
+    {
+        $initialToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'plugin.tx_example',
+        ];
+        $lastToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'settings',
+        ];
+        $expectedResult = $lastToken;
+        $expectedResult['fqObjectIdentifier'] = 'plugin.tx_example.settings';
+
+        $identifier = new FqObjectIdentifier();
+        $identifier->addPathSegment($initialToken);
+        $identifier->handleOpeningBrace();
+        $identifier->addPathSegment($lastToken);
+
+        $this->assertEquals(
+            $expectedResult,
+            $lastToken,
+            'Adding path segment does not add FQObjectIdentifier to token.'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addingPathSegment2ndTimeAddsFqToNewToken()
+    {
+        $initialToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'plugin.tx_example',
+        ];
+        $firstToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'settings',
+        ];
+        $lastToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'someSetting',
+        ];
+        $expectedResult = $lastToken;
+        $expectedResult['fqObjectIdentifier'] = 'plugin.tx_example.settings.someSetting';
+
+        $identifier = new FqObjectIdentifier();
+        $identifier->addPathSegment($initialToken);
+        $identifier->handleOpeningBrace();
+        $identifier->addPathSegment($firstToken);
+        $identifier->handleOpeningBrace();
+        $identifier->addPathSegment($lastToken);
+
+        $this->assertEquals(
+            $expectedResult,
+            $lastToken,
+            'Adding path segment does not add FQObjectIdentifier to token on 2nd call.'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function openingAndClosingBracesWillAdjustPath()
+    {
+        $initialToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'plugin.tx_example',
+        ];
+        $firstToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'settings',
+        ];
+        $secondToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'someSetting',
+        ];
+        $lastToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'view',
+        ];
+        $expectedResult = $lastToken;
+        $expectedResult['fqObjectIdentifier'] = 'plugin.tx_example.view';
+
+        $identifier = new FqObjectIdentifier();
+        $identifier->addPathSegment($initialToken);
+        $identifier->handleOpeningBrace();
+        $identifier->addPathSegment($firstToken);
+        $identifier->handleOpeningBrace();
+        $identifier->addPathSegment($secondToken);
+        $identifier->handleClosingBrace();
+        $identifier->addPathSegment($lastToken);
+
+        $this->assertEquals(
+            $expectedResult,
+            $lastToken,
+            'Curly braces do not modify path as expected.'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function AddingPathSegmentAfterAnotherResetsPath()
+    {
+        $initialToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'plugin.tx_example.settings.someThing',
+        ];
+        $lastToken = [
+            'type' => TokenInterface::TYPE_OBJECT_IDENTIFIER,
+            'content' => 'plugin.tx_example.settings.anotherOne',
+        ];
+        $expectedResult = $lastToken;
+        $expectedResult['fqObjectIdentifier'] = $expectedResult['content'];
+
+        $identifier = new FqObjectIdentifier();
+        $identifier->addPathSegment($initialToken);
+        $identifier->addPathSegment($lastToken);
+
+        $this->assertEquals(
+            $expectedResult,
+            $lastToken,
+            'Adding path segment without braces in between resets.'
+        );
+    }
+}
